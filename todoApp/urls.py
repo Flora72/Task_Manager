d900e4967ab2f8c5224cd4ac50c3d54b33d11062
Flora72 <zahratheo569@gmail.com>
2025-10-21 15:07:40 +0300

    CRUD functionality routing

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('todo_list/', views.todo_list, name='todo_list'),
+    path('create/', views.create_task, name='create_task'),
+    path('update/<int:id>/', views.update_task, name='update_task'),
+    path('delete/<int:id>/', views.delete_task, name='delete_task'),
 ]
