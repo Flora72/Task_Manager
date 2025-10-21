4e62d2721a7ac9c2297fcce71cd1e8373aa0c255
Flora72 <zahratheo569@gmail.com>
2025-10-21 14:59:49 +0300

    create task view

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import List
+from .forms import ListForm
+
+
 def index(request):
     return render(request, 'index.html')
 
 def todo_list(request):
     return render(request, 'todo_list.html')
 
+# creates the task
+
+def create_task(request):
+    if request.method == 'POST':
+        form = ListForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+        else:
+            form = ListForm()
+            return render(request, 'todo_form.html', {'form': form})
