f1dbceb1be0686a292048a09d8061e8f8556f338
Flora72 <zahratheo569@gmail.com>
2025-10-21 15:05:06 +0300

    update and delete views

--- todoApp/views.py
+++ todoApp/views.py
@@ -20,4 +20,23 @@ def create_task(request):
             return redirect('todo_list')
         else:
             form = ListForm()
-            return render(request, 'todo_form.html', {'form': form})
+    return render(request, 'todo_form.html', {'form': form})
+
+
+# updates the saved tasks
+def update_task(request, task_id):
+    task = get_object_or_404(List, id=task_id)
+    if request.method == 'POST':
+        form = ListForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+        else:
+            form = ListForm(instance=task)
+    return render(request, 'todo_form.html', {'form': form})
+
+# deletes the saved or completed tasks
+def delete_task(request, task_id):
+    task = get_object_or_404(List, id=task_id)
+    task.delete()
+    return redirect('todo_list')
